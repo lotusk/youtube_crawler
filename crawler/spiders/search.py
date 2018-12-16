103d94b78474be04c2633dd7e3896d41fc650491
Lucifer <lethexx@gmail.com>
2018-12-16 23:47:44 +0800

    fix problem 3

--- crawler/spiders/search.py
+++ crawler/spiders/search.py
@@ -13,6 +13,7 @@ class YBSearchSpider(scrapy.Spider):
         raise
 
     url_template = "https://www.googleapis.com/youtube/v3/search?part=snippet&videoCategoryId=27&type=video&"
+    # url_related = "https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&"
     max_result = 50
     # download_delay = 10000/3600.0  # Throttle  set in settings.py
 
@@ -35,11 +36,25 @@ class YBSearchSpider(scrapy.Spider):
             self.url_template + urllib.parse.urlencode({"q": keyword, "maxResults": self.max_result, "key": self.key})
             for keyword in
             self.keywords]
-        # urls = urls[:1]  # I think scrapy will have a config about max request , find it later
+        urls = urls[:1]  # I think scrapy will have a config about max request , find it later
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
+    def just_parse_items(self, response):
+        jsonresponse = json.loads(response.body_as_unicode())
+        items = jsonresponse.get("items", [])
+        for item in items:
+            yield item
+
     def parse(self, response):
         jsonresponse = json.loads(response.body_as_unicode())
-        for item in jsonresponse.get("items", []):
+        items = jsonresponse.get("items", [])
+        for item in items:
             yield item
+
+        for item in items:
+            related_id = item.get("id").get("videoId")
+            print("related_id:%s" % related_id)
+            url = self.url_template + urllib.parse.urlencode(
+                {"relatedToVideoId": related_id, "maxResults": self.max_result, "key": self.key})
+            yield scrapy.Request(url, callback=self.just_parse_items)
